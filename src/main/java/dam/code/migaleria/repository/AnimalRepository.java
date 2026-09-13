f0e3bb8b3654a46c60cb8b2488be246f6848bbfe
adampda <ofi.adam01@gmail.com>
2026-09-13 23:39:20 +0200

    Change AnimalRepository to use MongoDB

--- src/main/java/dam/code/migaleria/repository/AnimalRepository.java
+++ src/main/java/dam/code/migaleria/repository/AnimalRepository.java
@@ -1,9 +1,9 @@
 package dam.code.migaleria.repository;
 
 import dam.code.migaleria.model.Animal;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AnimalRepository extends JpaRepository<Animal,Long> {
+public interface AnimalRepository extends MongoRepository<Animal, String> {
 }
